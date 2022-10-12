b25ea20bf74e9ad965c9421af749e5538b927970
maxiang <maxiang@haoshuo.com>
2022-10-12 18:35:57 +0800

    feat: add go to home

--- telegram-media-app/ContentView.swift
+++ telegram-media-app/ContentView.swift
@@ -10,17 +10,19 @@ import PermissionsKit
 import PhotoLibraryPermission
 
 struct ContentView: View {
-//    @StateObject var RootVM = RootViewModal()
+    @State private var isShown = Permission.photoLibrary.authorized
+    
     init() {
         let authorized = Permission.photoLibrary.authorized
-        print(authorized)
-//        RootVM.userFlow = .allowAccess
     }
     var body: some View {
         Group {
-            AllowAccess()
-              }
-       
+            if(isShown){
+                HomeView()
+            }else {
+                AllowAccess()
+            }
+        }
     }
 }
 
@@ -28,12 +30,4 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
-
-class RootViewModal: ObservableObject {
-    enum UserFlow {
-        case allowAccess
-        case home
-    }
-    @Published var userFlow: UserFlow = .allowAccess
 }
